36afbee50ab1019e1445b652a3e729dce21c6243
Patricia S <patriciasteffen@gmx.ch>
2021-08-29 09:58:49 +0200

    before forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-app';
-  loadedSelected = 'recipes';
 
-  onNavigate(selected: string) {
-    this.loadedSelected = selected;
-  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+
 
   private recipes: Recipe[] = [
     new Recipe('Tasty Schnitzel', 'This is simply a test', 'https://live.staticflickr.com/8283/7717719952_5377f33781_b.jpg', [
